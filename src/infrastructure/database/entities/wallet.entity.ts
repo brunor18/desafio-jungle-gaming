751616dac0bc4b5ff8136964a5c7d6d7be36ae09
Bruno <brunoro343@gmail.com>
2026-08-28 00:12:51 -0300

    feat: adicionando wallet entity

---
+++ src/infrastructure/database/entities/wallet.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryKey, Property, } from '@mikro-orm/decorators/legacy';
+
+
+@Entity({ tableName: 'wallets' })
+export class WalletEntity {
+    @PrimaryKey({ type: 'uuid', defaultRaw: 'gen_random_uuid()' })
+    id!: string;
+
+    @Property()
+    userId!: string;
+
+    @Property()
+    currency!: string;
+
+    @Property({
+        type: 'decimal',
+        precision: 20,
+        scale: 2,
+    })
+    balance!: string;
+
+    @Property()
+    version!: number;
+
+    @Property()
+    createdAt: Date = new Date(); // separei os timestamps da classe principal pois esse dado so e usado para controle de persistencia
+
+    @Property({ onUpdate: () => new Date() })
+    updatedAt: Date = new Date();
+}
